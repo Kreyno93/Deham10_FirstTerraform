487eabec151e7b74b4d02c3a30fc86da0722e1e8
Kreyno93 <michael.wagener@neuefische.de>
2024-02-07 14:53:02 +0100

    important function has been added to the file

--- main.tf
+++ main.tf
@@ -93,17 +93,29 @@ resource "aws_instance" "Deham10" {
             margin: 0 auto; /* Center the image horizontally */
             margin-top: 20px; /* Add some space between the button and the image */
         }
+
+        .youtube-link {
+            display: none; /* Hide the YouTube link by default */
+        }
     </style>
 </head>
 <body>
     <div class="container">
         <h1>Welcome to the Future</h1>
         <p>This is a futuristic HTML template. Feel free to customize it according to your needs.</p>
-        <button>Get Started</button>
+        <button onclick="redirectToYouTube()">Get Started</button>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="youtube-link" id="youtube-link">Watch Video</a>
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.KPGtExKO5D314w3ZOxNtlgHaEK%26pid%3DApi&f=1&ipt=17af43335f179ffb7f415d8f7465df8211d6018bf2c06c246fd2af0cf8dc5ba3&ipo=images">
     </div>
+
+    <script>
+        function redirectToYouTube() {
+            window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'; // Redirect to the YouTube link
+        }
+    </script>
 </body>
 </html>
+
 
                                                 " > /var/www/html/index.html
                                 EOF
