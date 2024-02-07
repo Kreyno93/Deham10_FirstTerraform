34464b783411cb54959fd0177ca677bdeec44f28
Kreyno93 <michael.wagener@neuefische.de>
2024-02-07 14:29:50 +0100

    adding elastic ip

--- main.tf
+++ main.tf
@@ -108,6 +108,10 @@ resource "aws_instance" "Deham10" {
     }
 }
 
+resource "aws_eip" "Deham10" {
+    instance = aws_instance.Deham10.id
+}
+
 output "Instance_Id" {
     description = "The ID of the instance"
     value       = aws_instance.Deham10.id
