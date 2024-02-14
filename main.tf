e7869ba28553db8faba7aa696f37c9e1efe49ae9
Kreyno93 <michael.wagener@neuefische.de>
2024-02-14 11:28:30 +0100

    adjusting version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         aws = {
         source  = "hashicorp/aws"
-        version = "5.35.0"
+        version = "~> 5.35.0"
         }
     }
     
@@ -10,7 +10,6 @@ terraform {
 provider "aws" {
     profile = "default"
     region = "us-west-2"
-
 }
 
 resource "aws_security_group" "Deham10" {
